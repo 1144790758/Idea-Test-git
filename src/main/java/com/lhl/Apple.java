865821283aac5688e97a60c59f98201b5d010fd4
tom_glb <goodMoning_glb@123.com>
2020-04-13 22:56:48 +0800

    Idea-Test-git first local commit !!

---
+++ src/main/java/com/lhl/Apple.java
@@ -0,0 +1,12 @@
+package com.lhl;
+
+/**
+ * @athor:lhl
+ * @create:2020-04-13 22:47
+ */
+public class Apple {
+    public static void main(String[] args) {
+
+        System.out.println("I have an apple !");
+    }
+}
---
+++ src/test/java/com/lhl/Pencil.java
@@ -0,0 +1,11 @@
+package com.lhl;
+
+/**
+ * @athor:lhl
+ * @create:2020-04-13 22:48
+ */
+public class Pencil {
+    public static void main(String[] args) {
+        System.out.println("I have a pencil~");
+    }
+}
